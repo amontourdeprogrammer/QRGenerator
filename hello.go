c1f3f484b4abfbf6841480f7aaf1dbf60ea72bee
Anna-Livia <annalivia.cardin@gmail.com>
2017-04-03 17:04:24 +0200

    creates test file and creates empty png

--- hello.go
+++ hello.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"os"
+	"log"
+)
+
+
+func GenerateQRCode(w io.Writer, code string, version Version) error {
+	size := version.PatternSize()
+	img := image.NewNRGBA(image.Rect(0,0,size,size))
+	return png.Encode(w,img)
+	}
+
+type Version int8
+func (v Version) PatternSize() int {
+ 	return 4*int(v) + 17
+	}
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+	file, err := os.Create("qrcode.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	err = GenerateQRCode(file, "555-2368", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
