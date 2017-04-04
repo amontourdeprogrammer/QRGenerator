4f2646fbd13ad7fd87cccc1db5d4e2d07a385292
Lorena Rangel <rangel.loreen@gmail.com>
2017-04-04 14:49:41 +0200

    Generates QR code from a String

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/skip2/go-qrcode"
 	"fmt"
 	"image"
 	"image/png"
@@ -10,30 +11,15 @@ import (
 	"net/http"
 )
 
-
-func GenerateQRCode(w io.Writer, code string, version Version) error {
-	size := version.PatternSize()
-	img := image.NewNRGBA(image.Rect(0,0,size,size))
-	return png.Encode(w,img)
-	}
-
-type Version int8
-func (v Version) PatternSize() int {
- 	return 4*int(v) + 17
-	}
-
 func main() {
 	fmt.Printf("Hello, world.\n")
-	file, err := os.Create("qrcode.png")
-	if err != nil {
+	var png []byte
+	png, err := qrcode.Encode("Hello", qrcode.Medium, 256)
+	err = qrcode.WriteFile("Hello", qrcode.Medium, 256, "qr.png")
+	if err!= nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
-
-	err = GenerateQRCode(file, "555-2368", 1)
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = png
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world",)
