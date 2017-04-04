4dab4a0cde663bfa8c36de5cd1afab6810626bc9
SarahBu07 <mbp2010@macbook-pro-de-mbp2010.home>
2017-04-04 16:13:22 +0200

    QR code function updated

--- hello.go
+++ hello.go
@@ -3,26 +3,25 @@ package main
 import (
 	"github.com/skip2/go-qrcode"
 	"fmt"
-	"image"
-	"image/png"
-	"io"
-	"os"
 	"log"
 	"net/http"
 )
 
-func main() {
-	fmt.Printf("Hello, world.\n")
-	var png []byte
-	png, err := qrcode.Encode("Hello", qrcode.Medium, 256)
-	err = qrcode.WriteFile("Hello", qrcode.Medium, 256, "qr.png")
+func GenerateQRcode(phrase string) {
+	err := qrcode.WriteFile(phrase, qrcode.Medium, 256, "qr.png")
+	fmt.Printf("err=", err)
+
 	if err!= nil {
 		log.Fatal(err)
 	}
-	_ = png
+}
 
+func main() {
+	fmt.Printf("Hello, world.\n")
+	GenerateQRcode("je suis Sarah")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world",)
 	})
 	http.ListenAndServe(":8080", nil)
 }
+
