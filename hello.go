fe5d19927f8c21d9fd627748c02d4c709c45b0ef
Lorena Rangel <rangel.loreen@gmail.com>
2017-04-05 16:42:58 +0200

    Display image from a URL

--- hello.go
+++ hello.go
@@ -17,13 +17,21 @@ func GenerateQRcode(phrase string) {
 func HandlerBill (w http.ResponseWriter, r *http.Request){
 	URLparam := r.URL.Path[1:]
 	if len(URLparam) != 0 {
-		fmt.Fprintf(w, "Hello %s", URLparam )
-		GenerateQRcode(URLparam)
-		http.ServeFile(w, r, "qr.png")
+		if URLparam == "favicon.ico"{
+		fmt.Fprintf(w, "<html><img src='http://www.artsmartiauxcombat.com/images/favicon.ico-16x16-i11.ico'></html>")
+		}else{
+			if URLparam == "img/qr.png"{
+				fmt.Fprintf(w, "<html><img src='http://www.artsmartiauxcombat.com/images/favicon.ico-16x16-i11.ico'></html>")
+			}else{
+				GenerateQRcode(URLparam)
+				fmt.Println(URLparam)
+				fmt.Fprintf(w, "<h1>Hello</h1> <img src='http://localhost:8080/img/qr.png'> %s", URLparam )
+			}
 	} else {
 		fmt.Fprintf(w,"Hello bill !\nNothing to see here.\n")
-	}	
+	}
 }
+
 
 
 func main() {
