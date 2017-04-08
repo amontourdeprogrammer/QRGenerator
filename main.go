68cefd3a5b9c6d88b500783cbefc66744df66239
Anna-Livia <annalivia.cardin@gmail.com>
2017-04-09 00:54:35 +0200

    fixes the dl button

--- main.go
+++ main.go
@@ -19,14 +19,15 @@ func HandlerBill (w http.ResponseWriter, r *http.Request){
 	URLparam := r.URL.Path[1:]
 	if len(URLparam) != 0 {
 				GenerateQRcode(URLparam)
-				fmt.Fprintf(w, "<h1>Here is the QRcode for %s</h1> <a href='img/qr.png' download><button>Download!</button></a><img src='/img/qr.png'> ", URLparam )
+				fmt.Fprintf(w, "<h1>Here is the QRcode for %s</h1> <a href='/img/qr.png' download><button>Download!</button></a> <img src='/img/qr.png'> ", URLparam )
 			}else{
 		fmt.Fprintf(w,"Hello bill !\nNothing to see here.\n")
 	}
 }
 
 func HandlerFavicon (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"Hello I am Favicon!")
+	fmt.Print("demande de favicon")
+	http.Redirect(w, r, "/img/favicon.ico", http.StatusFound)
 
 }
 
@@ -38,9 +39,10 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 	http.HandleFunc("/", HandlerBill)
 	http.HandleFunc("/favicon.ico", HandlerFavicon)
-	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 	http.ListenAndServe(":"+ port, nil)
 
 }
