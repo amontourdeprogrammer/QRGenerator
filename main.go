eba7e0a35005c3d3347fe7817d883dc0f3d96b75
Anna-Livia <annalivia.cardin@gmail.com>
2017-04-09 02:01:21 +0200

    generates qr code dynamically, no more crezting a qr.png

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 	"os"
 )
 
-func GenerateQRcode(phrase string) {
-	err := qrcode.WriteFile(phrase, qrcode.Medium, 256, "img/qr.png")
+func GenerateQRcode(phrase string) ([]byte) {
+	png, err := qrcode.Encode(phrase, qrcode.Medium, 256)
 	if err!= nil {
 		log.Fatal(err)
 	}
+	return png
 }
 
 func HandlerBill (w http.ResponseWriter, r *http.Request){
 	URLparam := r.URL.Path[1:]
 	if len(URLparam) != 0 {
-				GenerateQRcode(URLparam)
-				fmt.Fprintf(w, "<h1>Here is the QRcode for %s</h1> <a href='/img/qr.png' download><button>Download!</button></a> <img src='/img/qr.png'> ", URLparam )
+				fmt.Fprintf(w, "<h1>Here is the QRcode for %s</h1> <a href='/img/%s' download><button>Download!</button></a> <img src='/img/%s'> ", URLparam, URLparam, URLparam )
 			}else{
 		fmt.Fprintf(w,"Hello bill !\nNothing to see here.\n")
 	}
@@ -31,6 +31,19 @@ func HandlerFavicon (w http.ResponseWriter, r *http.Request) {
 
 }
 
+func Handlerimage (w http.ResponseWriter, r *http.Request) {
+	URLparam := (r.URL.Path[5:])
+	writeImage(w, GenerateQRcode(URLparam))
+}
+
+func writeImage(w http.ResponseWriter, png []byte) {
+
+	w.Header().Set("Content-Type", "image/png")
+	if _, err := w.Write(png); err != nil {
+		log.Println("unable to write image.")
+	}
+}
+
 
 
 func main() {
@@ -40,7 +53,8 @@ func main() {
 		port = "8080"
 	}
 
-	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	//http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	http.HandleFunc("/img/", Handlerimage)
 	http.HandleFunc("/", HandlerBill)
 	http.HandleFunc("/favicon.ico", HandlerFavicon)
 	http.ListenAndServe(":"+ port, nil)
